f0fdf916e7115c771382ab00597d826848a3b680
1930158 <1930158@01-18122.pusd.dom>
2024-12-11 13:49:29 -0800

    aoc d3 - bug fixes

--- adventOfCodeDay3/src/adventOfCodeDay3/Index.java
+++ adventOfCodeDay3/src/adventOfCodeDay3/Index.java
@@ -1,11 +1,37 @@
 package adventOfCodeDay3;
 import java.io.File;
+import java.util.regex.*;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.*;
 
 
 public class Index {
+	
+	
+	/*
+	 * Extract the next mult command from the given line
+	 *
+	 * Returns an empty string if none exists
+	 */
+	public String extractMult(String line) {
+		
+		if(line.indexOf("mul") == -1) 
+		{
+			return ""; 							//not a valid command found in line :>
+		}
+		else
+		{
+			int start = line.indexOf("mul");
+			int end = line.indexOf(")");
+			
+			//STOP! if there is no closing ")" then
+			//this is not the one we want :>
+			
+			return line.substring(start,end);
+		}
+	}
+	
 	
 	
 	
@@ -29,9 +55,28 @@ public class Index {
 				
 				
 				//grab 1 line
-				System.out.println(scan.next());
-				System.out.println("---------------");
 			
+				
+				String line = scan.next();
+				String regex = "mul\\((\\d+),(\\d+)\\)";
+				
+				Pattern pattern = Pattern.compile(regex);
+				Matcher matcher = pattern.matcher(line);
+				
+				List<String> validCalls = new ArrayList<>();
+				
+				while(matcher.find()) {
+					
+					validCalls.add(matcher.group());
+					
+					
+					
+				}
+				
+				System.out.println("Valid 'mul' calls:");
+				for(String call : validCalls) {
+					System.out.println(call);
+				}
 		
 		
 				//split the line into a string array
@@ -40,7 +85,8 @@ public class Index {
 				
 			}
 			
-			System.out.println(count);
+			scan.close();
+	
 			
 
 		} catch (FileNotFoundException e) {
